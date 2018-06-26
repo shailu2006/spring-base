c41549cb5a1c72a9c2ab0b874a0fd52cb5cd4ffc
Shailesh Kumar D <shailu2006@gmail.com>
2018-06-27 04:04:36 +0530

    Spring read a property file project checkin

---
+++ spring-core/read-property-file/sp-core-readprops/src/main/java/com/shailesh/spcore/spcorereadprops/SpCoreReadpropsApplication.java
@@ -0,0 +1,12 @@
+package com.shailesh.spcore.spcorereadprops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCoreReadpropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCoreReadpropsApplication.class, args);
+	}
+}
